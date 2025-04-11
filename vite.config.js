9e5026d6acbd10ded4d1542b0bdbb4b312e1c41c
krishnaKantVishWakarma-titan <krishnakantvish.24@gmail.com>
2025-04-11 18:33:29 +0530

    ed

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/rootwater/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
